aaf5599c2f74d261e002ee0fc13a723e17f038af
se0hyun <kimlisa00@seoultech.ac.kr>
2023-08-07 12:14:38 +0900

    반응형으로 변경

--- chat2.py
+++ chat2.py
@@ -1,9 +1,12 @@
-# gradio 사용하기
+import openai
 import gradio as gr
+openai.api_key = "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"
 
-def greet(name):
-    return "Hello " + name + "!"    #내가 입력한 값까지 합쳐서 출력
+def greet(content):
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo", messages=[{"role": "user", "content": content}]
+    )
+    return completion.choices[0].message.content
 
 demo = gr.Interface(fn=greet, inputs="text", outputs="text")
-
-demo.launch(share=True)   
+demo.launch(share=True)
